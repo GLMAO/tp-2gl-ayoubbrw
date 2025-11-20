725a6015b640011b4cf6ab4472a0fb4d6da1f536
bouraoui <bouraouiayoub66@exemple.com>
2025-11-20 14:01:29 +0100

    c'est la fin

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -2,13 +2,24 @@ package com.polytech.tp;
 
 public class CoursBuilder {
     // TODO: Implémenter le pattern Builder
+    private String matiere;
+    private String enseignant;
+    private String salle;
+    private String date;
+    private String heureDebut;
+    private boolean estOptionnel;
+    private String niveau;
+    private boolean necessiteProjecteur;
     
     
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
+    public CoursBuilder setMatiere(String matiere) { this.matiere = matiere;
+        return this; }
+    public CoursBuilder setEnseignant(String enseignant) { this.enseignant = enseignant;
+        return this; }
     
     
     public Cours build() {
-        return null;
+        return new Cours(matiere, enseignant, salle, date, heureDebut,
+                estOptionnel, niveau, necessiteProjecteur);
     }
 }
