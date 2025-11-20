7b0e4cd42a7660a59dc9f3ccfb2b97ecb4a46b43
bouraoui <bouraouiayoub66@exemple.com>
2025-11-20 14:30:20 +0100

    c'est la fin

--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -16,6 +16,35 @@ public class CoursBuilder {
         return this; }
     public CoursBuilder setEnseignant(String enseignant) { this.enseignant = enseignant;
         return this; }
+    public CoursBuilder setSalle(String salle) {
+        this.salle = salle;
+        return this;
+    }
+
+    public CoursBuilder setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public CoursBuilder setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+        return this;
+    }
+
+    public CoursBuilder setEstOptionnel(boolean estOptionnel) {
+        this.estOptionnel = estOptionnel;
+        return this;
+    }
+
+    public CoursBuilder setNiveau(String niveau) {
+        this.niveau = niveau;
+        return this;
+    }
+
+    public CoursBuilder setNecessiteProjecteur(boolean necessiteProjecteur) {
+        this.necessiteProjecteur = necessiteProjecteur;
+        return this;
+    }
     
     
     public Cours build() {
